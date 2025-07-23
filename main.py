0288cf7670def8515962b9e7668449f907dc4b1a
mscheer314 <mscheer314@gmail.com>
2025-07-22 19:39:35 -0500

    add count_chars

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import count_words
+from stats import count_chars
 
-with open("./books/franenstein.txt") as f:
+with open("./books/frankenstein.txt") as f:
     file_contents = f.read()
 
 count_words(file_contents)
+count_chars(file_contents)
--- stats.py
+++ stats.py
@@ -2,4 +2,13 @@ def count_words(file_contents):
     word_count = file_contents.split(" ")
     print("%s words found in Frankenstein" % len(word_count))
 
+def count_chars(file_contents):
+    char_counts = {}
+    for char in file_contents:
+        if (char.lower() not in char_counts):
+            char_counts.update({char.lower(): 1})
+        else:
+            char_counts.update({char.lower(): char_counts.get(char.lower()) + 1})
+    print(char_counts)
+
 
